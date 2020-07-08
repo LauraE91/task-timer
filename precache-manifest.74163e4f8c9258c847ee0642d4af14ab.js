2352dba24ce3df906ef6f79cc992b3b54d9af477
LauraE2020 <smalltowngirl591@gmail.com>
2020-07-08 17:07:00 -0400

    Updates

---
+++ precache-manifest.74163e4f8c9258c847ee0642d4af14ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb808456a5b2602599daa09c97d3584c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae91d4f7dfafb22a0e71",
+    "url": "/static/css/main.ee60f0d4.chunk.css"
+  },
+  {
+    "revision": "ed7a042713c248e24d18",
+    "url": "/static/js/2.d58e5255.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d58e5255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae91d4f7dfafb22a0e71",
+    "url": "/static/js/main.8c5f1436.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
